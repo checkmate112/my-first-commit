f8c915f03e5c6a80a8d0d3d7060e00adade72ef5
checkmate112 <csmax007@gmail.com>
2019-03-18 17:28:28 +0530

    django project 1

---
+++ abc.py
@@ -0,0 +1,4 @@
+dict={'Name':'Zara','Age':7}
+word=dict.items()
+for w,value in word:
+    print(value) 
--- wordcount/views.py
+++ wordcount/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
 def homepage(request):
     return render(request,'abc.html')
 
 def count(request):
-    return render(request,'count.html')
+    data=request.GET['fulltextarea']
+    word_list=data.split()
+    listlength = len(word_list)
+    worddictionary={}
+    for word in word_list:
+        if word in worddictionary:
+            worddictionary[word]+=1
+        else:
+            worddictionary[word]=1
+    sorted_list=sorted(worddictionary.items(),key=operator.itemgetter(1),reverse=True)
+    return render(request,'count.html',{'fulltext':data,'words':listlength,'wordcount':sorted_list})
